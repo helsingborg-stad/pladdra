5e70faab4bcde4a02a8316e08e1fad3bf78e28c2
nRamstedt <nikolas.ramstedt@gmail.com>
2021-06-23 23:18:39 +0200

    add file utils

---
+++ admin-ui/src/utils/File.ts
@@ -0,0 +1,21 @@
+import prettyBytes from 'pretty-bytes'
+
+export const humanReadableToBytes = (humanReadable: string) : number => {
+	const units: Record<string, any> = {
+		B: (n: number) => n / 1000,
+		kB: (n: number) => n,
+		MB: (n: number) => n * 1000,
+		GB: (n: number) => n * 1000 * 1000,
+		TB: (n: number) => n * 1000 * 1000 * 1000,
+	}
+
+	const [ size, unit ] = humanReadable.replace(',', '').split(' ')
+
+	return  units[unit](Number(size))
+}
+
+export const bytesToHumanReadable = (
+	bytes: number, 
+	options: Record<string, any> = { minimumFractionDigits: 3 }) : string => prettyBytes(bytes, options)
+	
+export const parseExtension = (s: string): string | null => s.split('.').pop() ?? null
