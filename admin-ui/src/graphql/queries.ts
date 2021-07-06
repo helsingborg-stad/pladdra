264e8ea4fc914c2cb66313133bc642cd764f56c4
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-07-06 11:50:43 +0200

    codegen

--- admin-ui/src/graphql/queries.ts
+++ admin-ui/src/graphql/queries.ts
@@ -2,6 +2,42 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
+      id
+      name
+      description
+      image {
+        bucket
+        key
+        region
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
 export const getAsset = /* GraphQL */ `
   query GetAsset($id: ID!) {
     getAsset(id: $id) {
@@ -44,37 +80,33 @@ export const listAssets = /* GraphQL */ `
     }
   }
 `;
-export const getTodo = /* GraphQL */ `
-  query GetTodo($id: ID!) {
-    getTodo(id: $id) {
-      id
-      name
-      description
-      image {
-        bucket
-        key
-        region
-      }
-      createdAt
-      updatedAt
-      owner
-    }
-  }
-`;
-export const listTodos = /* GraphQL */ `
-  query ListTodos(
-    $filter: ModelTodoFilterInput
+export const assetsByFileFormat = /* GraphQL */ `
+  query AssetsByFileFormat(
+    $fileFormat: AssetFileFormat
+    $createdAtUpdatedAt: ModelAssetByFileFormatCompositeKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelAssetFilterInput
     $limit: Int
     $nextToken: String
   ) {
-    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    assetsByFileFormat(
+      fileFormat: $fileFormat
+      createdAtUpdatedAt: $createdAtUpdatedAt
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
       items {
         id
         name
+        type
+        fileFormat
+        fileName
+        fileSize
         description
         createdAt
         updatedAt
-        owner
       }
       nextToken
     }
