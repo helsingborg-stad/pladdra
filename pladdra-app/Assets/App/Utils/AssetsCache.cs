fd0d9e1fface64b79adc779e8cc75b8aaa7b59a8
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-07-12 20:09:18 +0200

    add namespace & clean up imports

--- pladdra-app/Assets/App/Utils/AssetsCache.cs
+++ pladdra-app/Assets/App/Utils/AssetsCache.cs
@@ -1,42 +1,39 @@
-using System;
-using System.Linq;
 using System.Reflection;
-using System.Reflection.Emit;
 using System.Collections.Generic;
-using UnityEngine;
 using Newtonsoft.Json;
 
-// Use this class to store user session data to use for refreshing tokens
-[System.Serializable]
-public class AssetsCache : ISaveable
-
+namespace Pladdra
 {
-    public List<Pladdra.API.Types.Asset> items = new List<Pladdra.API.Types.Asset>();
+    // Use this class to store user session data to use for refreshing tokens
+    [System.Serializable]
+    public class AssetsCache : ISaveable
 
-    public string updatedAt;
-
-    public AssetsCache() { }
-
-    public string ToJson()
     {
+        public List<Pladdra.API.Types.Asset> items = new List<Pladdra.API.Types.Asset>();
+        public string updatedAt;
 
-        return JsonConvert.SerializeObject(this);
-    }
+        public AssetsCache() { }
 
-    public void LoadFromJson(string jsonToLoadFrom)
-    {
-        AssetsCache jsonData = JsonConvert.DeserializeObject<AssetsCache>(jsonToLoadFrom);
-        var fields = this.GetType().GetFields();
-        foreach (var field in jsonData.GetType().GetFields())
+        public string ToJson()
         {
-            var value = field.GetValue(jsonData);
-            if (value != null)
-                this.GetType().GetField(field.Name).SetValue(this, value);
+            return JsonConvert.SerializeObject(this);
         }
-    }
 
-    public string FileNameToUseForData()
-    {
-        return "assets.json";
+        public void LoadFromJson(string jsonToLoadFrom)
+        {
+            AssetsCache jsonData = JsonConvert.DeserializeObject<AssetsCache>(jsonToLoadFrom);
+        var fields = this.GetType().GetFields();
+            foreach (var field in jsonData.GetType().GetFields())
+            {
+                var value = field.GetValue(jsonData);
+                if (value != null)
+                    this.GetType().GetField(field.Name).SetValue(this, value);
+            }
+        }
+
+        public string FileNameToUseForData()
+        {
+            return "assets.json";
+        }
     }
 }
