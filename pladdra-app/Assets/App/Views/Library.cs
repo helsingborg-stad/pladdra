bfa4bdcc212f0737ef0c9170d4aacfeb683b19b9
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-07-14 18:01:24 +0200

    render asset items from cache data & add click events

--- pladdra-app/Assets/App/Views/Library.cs
+++ pladdra-app/Assets/App/Views/Library.cs
@@ -1,17 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class Library : View
+
+namespace Pladdra.Views
 {
-    public Button backButton;
-
-    public override void Initialize()
+    public class Library : View
     {
-        backButton.onClick.AddListener(onClickBackButton);
-    }
+        public Button backButton;
 
-    private void onClickBackButton()
-    {
-        ViewManager.ShowLast();
+        public GridLayoutGroup assetGrid;
+
+        private List<GameObject> items;
+
+        public GameObject assetItemPrefab;
+
+        private List<Pladdra.API.Types.Asset> itemsToRender;
+
+        public override void Initialize()
+        {
+            backButton.onClick.AddListener(onClickBackButton);
+        }
+
+        private void OnEnable()
+        {
+            if (itemsToRender == null || itemsToRender.Count == 0)
+            {
+                itemsToRender = AssetsManager.GetAssets();
+            }
+
+            if (itemsToRender.Count > 0 && itemsToRender.Count != items.Count)
+            {
+                itemsToRender.ForEach(InstantiateItem);
+            }
+        }
+
+        private void InstantiateItem(Pladdra.API.Types.Asset asset)
+        {
+            GameObject newObj = (GameObject)Instantiate(assetItemPrefab, assetGrid.gameObject.transform);
+            AssetGridItem item = newObj.GetComponent<AssetGridItem>();
+
+            item.titleComponent.text = asset.name;
+            item.metaComponent.text = asset.id;
+
+            item.buttonComponent.onClick.AddListener(() => onClickItem(asset));
+
+            items.Add(newObj);
+        }
+
+        private void onClickItem(Pladdra.API.Types.Asset asset)
+        {
+            Debug.Log(asset);
+        }
+
+        private void onClickBackButton()
+        {
+            ViewManager.ShowLast();
+        }
     }
 }
