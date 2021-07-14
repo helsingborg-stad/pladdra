84d157df650c83a9cd71168b0303816cd1761e7d
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-07-14 17:58:57 +0200

    add AssetGridItem component

---
+++ pladdra-app/Assets/App/Components/AssetGridItem.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+namespace Pladdra
+{
+    public class AssetGridItem : MonoBehaviour
+    {
+        public Button buttonComponent;
+        public Text titleComponent;
+        public Text metaComponent;
+    }
+}
