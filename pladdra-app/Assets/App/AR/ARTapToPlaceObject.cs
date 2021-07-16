404119edd8b6665e5edece41996a686558694e20
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-07-16 12:19:45 +0200

    make sure we dont hit an UI element when raycasting on touch

--- pladdra-app/Assets/App/AR/ARTapToPlaceObject.cs
+++ pladdra-app/Assets/App/AR/ARTapToPlaceObject.cs
@@ -8,10 +8,9 @@ using UnityEngine.XR.ARSubsystems;
 [RequireComponent(typeof(ARRaycastManager))]
 public class ARTapToPlaceObject : MonoBehaviour
 {
-    public bool isActive = false;
-
     public GameObject gameObjectToInstantiate;
-    public GameObject spawnedObject;
+    public static bool isActive;
+    public static GameObject spawnedObject;
     private ARRaycastManager _arRaycastManager;
     private Vector2 touchPosition;
 
@@ -35,25 +34,53 @@ public class ARTapToPlaceObject : MonoBehaviour
         return false;
     }
 
+    bool TouchCollideWithUI(Vector2 position)
+    {
+        UnityEngine.EventSystems.PointerEventData pointer = new UnityEngine.EventSystems.PointerEventData(UnityEngine.EventSystems.EventSystem.current);
+        pointer.position = position;
+        List<UnityEngine.EventSystems.RaycastResult> raycastResults = new List<UnityEngine.EventSystems.RaycastResult>();
+        UnityEngine.EventSystems.EventSystem.current.RaycastAll(pointer, raycastResults);
+
+        if (raycastResults.Count > 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
     void Update()
     {
+        if (!isActive)
+        {
+            return;
+        }
+
         if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if (TouchCollideWithUI(touchPosition))
             return;
 
         if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
         {
             var hitPose = hits[0].pose;
 
-            if (spawnedObject == null)
+            Debug.Log(hitPose);
+
+            if (spawnedObject == null && gameObjectToInstantiate != null)
             {
+                Debug.Log("CREATE NEW OBJECT");
                 spawnedObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation);
             }
-            else
+            else if (spawnedObject != null)
             {
                 if (spawnedObject.activeSelf != true)
                 {
                     spawnedObject.SetActive(true);
                 }
+                Debug.Log("MOVE OBJECT");
                 spawnedObject.transform.position = hitPose.position;
             }
         }
