61f95a62ea5cb1eb4bdc27d279f264fb2b4bc376
Sven65 <Sven65@users.noreply.github.com>
2021-08-25 09:26:08 +0200

    Make rays hit plane

--- pladdra-app/Assets/Scripts/Components/CameraRaycast.cs
+++ pladdra-app/Assets/Scripts/Components/CameraRaycast.cs
@@ -1,27 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class CameraRaycast : MonoBehaviour {
-    public Camera gameCamera;
+    public Camera ARCamera;
     public GameObject arMarkerPrefab;
+    public ARPlaneManager arPlaneManager;
+
+    public ARRaycastManager m_RaycastManager;
+
+    public int layerMask = 1 << 10;
 
     GameObject createdARMarker;
 
+    private Vector2 screenCenter;
+
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
     // Start is called before the first frame update
     void Start() {
-        createdARMarker = Instantiate(arMarkerPrefab, Vector3.zero, Quaternion.identity);
+        //createdARMarker = Instantiate(arMarkerPrefab, Vector3.zero, Quaternion.identity);
+
+        screenCenter = new Vector2(Screen.currentResolution.width / 2, Screen.currentResolution.height / 2);
     }
 
-    // Update is called once per frame
-    void Update() {
+    void placeSphereAtPhysical() {
+        Ray ray = ARCamera.ScreenPointToRay(screenCenter);
         RaycastHit hit;
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        
-        if (Physics.Raycast(ray, out hit)) {
-            Transform objectHit = hit.transform;
-            
-            // Do something with the object that was hit by the raycast.
+
+
+        if (Physics.Raycast(ray, out hit, float.MaxValue)) {
+            Debug.Log("hit: "+hit.transform.name);
+            if (createdARMarker == null) {
+                createdARMarker = Instantiate(arMarkerPrefab, hit.point, Quaternion.identity);
+            } else {
+                createdARMarker.transform.position = hit.point;
+            }
         }
+    }
+ 
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        placeSphereAtPhysical();
     }
 }
