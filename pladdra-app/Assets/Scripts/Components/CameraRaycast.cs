cb835c11d7d8a760d4551a44dcb3cc216ee53331
Sven65 <Sven65@users.noreply.github.com>
2021-08-26 09:15:57 +0200

    Make marker follow camera

--- pladdra-app/Assets/Scripts/Components/CameraRaycast.cs
+++ pladdra-app/Assets/Scripts/Components/CameraRaycast.cs
@@ -1,39 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 
 public class CameraRaycast : MonoBehaviour {
     public Camera ARCamera;
     public GameObject arMarkerPrefab;
-    public ARPlaneManager arPlaneManager;
-
-    public ARRaycastManager m_RaycastManager;
-
-    public int layerMask = 1 << 10;
-
     GameObject createdARMarker;
 
-    private Vector2 screenCenter;
-
-
-    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
-
-    // Start is called before the first frame update
-    void Start() {
-        //createdARMarker = Instantiate(arMarkerPrefab, Vector3.zero, Quaternion.identity);
-
-        screenCenter = new Vector2(Screen.currentResolution.width / 2, Screen.currentResolution.height / 2);
-    }
 
     void placeSphereAtPhysical() {
-        Ray ray = ARCamera.ScreenPointToRay(screenCenter);
         RaycastHit hit;
 
 
-        if (Physics.Raycast(ray, out hit, float.MaxValue)) {
-            Debug.Log("hit: "+hit.transform.name);
+        if (Physics.Raycast(ARCamera.transform.position, ARCamera.transform.forward, out hit, float.MaxValue)) {
+            ARPlane plane = hit.transform.GetComponent<ARPlane>();
+
+            if (plane == null) return;
+
             if (createdARMarker == null) {
                 createdARMarker = Instantiate(arMarkerPrefab, hit.point, Quaternion.identity);
             } else {
