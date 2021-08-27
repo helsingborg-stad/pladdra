b8d30164c90cce0d7f66216e0c653d4dc9271e9b
nRamstedt <nikolas.ramstedt@gmail.com>
2021-08-27 08:49:44 +0200

    add CreateWorkspaceView

---
+++ pladdra-app/Assets/Scripts/Views/CreateWorkspaceView.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
+
+
+using Pladdra.MVC.Models;
+using Pladdra.MVC.Controllers;
+using Pladdra.Components;
+
+namespace Pladdra.MVC.Views
+{
+    [RequireComponent(typeof(WithReRender))]
+    public class CreateWorkspaceView : View
+    {
+        private ICreateWorkspaceModel context;
+        private ICreateWorkspaceController controller;
+        public UnityEvent render;
+        public WithReRender withReRender;
+        public TMP_InputField nameField;
+        public Button createButton;
+        public Button backButton;
+
+        public override void Initialize()
+        {
+            withReRender = GetComponent<WithReRender>();
+            render = withReRender.renderEvent;
+
+            context = new CreateWorkspaceModel();
+            controller = new CreateWorkspaceController(context, render);
+
+            createButton.onClick.AddListener(controller.OnClickCreate);
+            backButton.onClick.AddListener(controller.OnClickBack);
+        }
+    }
+}
