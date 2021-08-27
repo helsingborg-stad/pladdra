0d02c789320cc46c0e0c407668a54b682e9e937d
nRamstedt <nikolas.ramstedt@gmail.com>
2021-08-27 08:47:24 +0200

    refactor menu view

--- pladdra-app/Assets/Scripts/Views/MenuView.cs
+++ pladdra-app/Assets/Scripts/Views/MenuView.cs
@@ -7,30 +7,33 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using TMPro;
 
+
 using Pladdra.MVC.Models;
 using Pladdra.MVC.Controllers;
 
 namespace Pladdra.MVC.Views
 {
-    public class MenuView : ViewWithRender
+    public class MenuView : View
     {
         private IMenuModel context;
         private IMenuController controller;
         private UnityEvent renderEvent;
         private bool shouldRender = true;
-        public Button startButton;
-        public Button downloadButton;
+        public Button createButton;
+        public Button listButton;
         public Button logoutButton;
         public Button devToolsButton;
 
         public override void Initialize()
         {
+            renderEvent = new UnityEvent();
             context = new MenuModel();
-            controller = new MenuController(context, renderEvent);
+            controller = new MenuController(context);
+            renderEvent.AddListener(() => shouldRender = true);
 
+            createButton.onClick.AddListener(controller.OnClickCreateWorkspace);
+            listButton.onClick.AddListener(controller.OnClickListWorkspace);
             logoutButton.onClick.AddListener(controller.OnClickLogout);
-            startButton.onClick.AddListener(controller.OnClickStart);
-            downloadButton.onClick.AddListener(controller.OnClickSync);
         }
     }
 }
