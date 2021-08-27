9a7b3cc165faeaf1c9389bd81344f740eb412412
nRamstedt <nikolas.ramstedt@gmail.com>
2021-08-27 08:48:32 +0200

    add create & list workspace models

---
+++ pladdra-app/Assets/Scripts/Models/CreateWorkspaceModel.cs
@@ -0,0 +1,19 @@
+using Pladdra.MVC.Views;
+using UnityEngine;
+
+
+namespace Pladdra.MVC.Models
+{
+    public interface ICreateWorkspaceModel
+    {
+        void CreateWorkspace();
+    }
+
+    public class CreateWorkspaceModel : ICreateWorkspaceModel
+    {
+        public void CreateWorkspace()
+        {
+
+        }
+    }
+}
---
+++ pladdra-app/Assets/Scripts/Models/ListWorkspaceModel.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+
+namespace Pladdra.MVC.Models
+{
+    public interface IListWorkspaceModel
+    {
+    }
+
+    public class ListWorkspaceModel : IListWorkspaceModel
+    {
+    }
+}
