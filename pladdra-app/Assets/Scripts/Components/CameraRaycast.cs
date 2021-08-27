97438fb5ef9f2b6451c7f6af858751d30f36e59c
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-08-27 15:27:13 +0200

    fix: remove MonoScript (depends on unity editor) & slim down

--- pladdra-app/Assets/Scripts/Components/CameraRaycast.cs
+++ pladdra-app/Assets/Scripts/Components/CameraRaycast.cs
@@ -3,30 +3,18 @@ using UnityEditor;
 using UnityEngine.Events;
 using System;
 
-namespace Pladdra.Components {
-    public class CameraRaycast : MonoBehaviour {
+namespace Pladdra.Components
+{
+    public class CameraRaycast : MonoBehaviour
+    {
         public UnityEvent<RaycastHit> onHitEvent;
-
         public Camera ARCamera;
-
-        [Tooltip("The component to look for in hit objects.\nIf specified, it only triggers the hit event if the hit object contains the component.\nIf not specified, it triggers the event on any hit.")]
-        public UnityEngine.Object targetType;
-
-        // Update is called once per frame
-        void FixedUpdate() {
+        void FixedUpdate()
+        {
             RaycastHit hit;
 
-            if (Physics.Raycast(ARCamera.transform.position, ARCamera.transform.forward, out hit, float.MaxValue)) {
-                if (targetType != null) {
-                    MonoScript script = targetType as MonoScript;
-                    Type scriptType = script.GetClass();
-                    Component outComponent;
-
-                    if(!hit.transform.TryGetComponent(out outComponent)) {
-                        return;
-                    }
-                }
-
+            if (Physics.Raycast(ARCamera.transform.position, ARCamera.transform.forward, out hit, float.MaxValue))
+            {
                 onHitEvent.Invoke(hit);
             }
         }
