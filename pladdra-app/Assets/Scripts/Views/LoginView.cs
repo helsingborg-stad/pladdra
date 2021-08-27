1f0cbf7cdc8f46f9c69e1c9ee2a0e226dcd918be
nRamstedt <nikolas.ramstedt@gmail.com>
2021-08-27 08:44:46 +0200

    implement WithReRender & remove ViewWithRender

--- pladdra-app/Assets/Scripts/Views/LoginView.cs
+++ pladdra-app/Assets/Scripts/Views/LoginView.cs
@@ -9,11 +9,15 @@ using TMPro;
 
 using Pladdra.MVC.Models;
 using Pladdra.MVC.Controllers;
+using Pladdra.Components;
 
 namespace Pladdra.MVC.Views
 {
-    public class LoginView : ViewWithRender
+
+    [RequireComponent(typeof(WithReRender))]
+    public class LoginView : View
     {
+        public UnityEvent render;
         private ILoginModel context;
         private ILoginController controller;
         private bool shouldRender = true;
@@ -21,11 +25,18 @@ namespace Pladdra.MVC.Views
         public TMP_InputField usernameFieldLogin;
         public TMP_InputField passwordFieldLogin;
         public TMP_Text noticeText;
+        WithReRender withReRender;
 
         public override void Initialize()
         {
+            withReRender = GetComponent<WithReRender>();
+            withReRender.OnRender += SetNoticeText;
+            withReRender.OnRender += ToggleUI;
+
+            render = withReRender.renderEvent;
+
             context = new LoginModel();
-            controller = new LoginController(context, renderEvent);
+            controller = new LoginController(context, render);
 
             loginButton.onClick.AddListener(() => controller.OnClickLogin(usernameFieldLogin.text, passwordFieldLogin.text));
         }
@@ -34,12 +45,6 @@ namespace Pladdra.MVC.Views
         {
             passwordFieldLogin.text = "";
             noticeText.text = "";
-        }
-
-        public override void OnRender()
-        {
-            SetNoticeText();
-            ToggleUI();
         }
 
         private void SetNoticeText()
