ef8176b169eed9db5ea04156918977740f4e97a2
nRamstedt <nikolas.ramstedt@gmail.com>
2021-08-29 04:07:31 +0200

    clean up

--- pladdra-app/Assets/Scripts/Components/WithReRender.cs
+++ pladdra-app/Assets/Scripts/Components/WithReRender.cs
@@ -11,7 +11,6 @@ namespace Pladdra.Components
             get
             {
 
-                Debug.Log(_renderEvent);
                 if (_renderEvent != null)
                 {
                     return _renderEvent;
