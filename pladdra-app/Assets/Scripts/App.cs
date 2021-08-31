d3608be43f0bceb3fe3535c3089850f49786a22d
nRamstedt <nikolas.ramstedt@gmail.com>
2021-08-31 11:14:59 +0200

    Initialize global models from App

--- pladdra-app/Assets/Scripts/App.cs
+++ pladdra-app/Assets/Scripts/App.cs
@@ -7,10 +7,12 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 using TMPro;
 using Pladdra.API;
+using Pladdra.MVC.Models;
+using System.Linq;
 
+using Pladdra.Core;
 namespace Pladdra
 {
-    [RequireComponent(typeof(AssetsManager))]
     [RequireComponent(typeof(Auth))]
     [RequireComponent(typeof(S3))]
     [RequireComponent(typeof(GraphQLClient))]
@@ -18,14 +20,64 @@ namespace Pladdra
     public class App : MonoBehaviour
     {
         public static string CachePath;
+        private List<IModel> models;
+        private static App _instance;
+        public static bool GetModel<T>(out T model) where T : class, IModel
+        {
+            model = null;
 
-        public static MVC.Models.WorkspaceModel workspaceModel;
+            for (int i = 0; i < _instance.models.Count; i++)
+            {
+                if (_instance.models[i] is T)
+                {
+                    model = (T)_instance.models[i];
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         void Awake()
         {
-            App.workspaceModel = new MVC.Models.WorkspaceModel();
+            _instance = this;
             CachePath = Application.persistentDataPath;
             Application.targetFrameRate = 30;
+            models = new List<IModel>();
+        }
+
+        void Start()
+        {
+            Models();
+            Views();
+        }
+
+        private void Models()
+        {
+            // Init your global models here
+            IModel[] instances = {
+                new AssetModel(),
+                new WorkspaceModel(),
+            };
+
+            for (int i = 0; i < instances.Length; i++)
+            {
+                // Do stuff with models after init
+                PopulateJsonModelData(instances[i]);
+            }
+
+            models.AddRange(instances);
+        }
+
+        private void PopulateJsonModelData(IModel model)
+        {
+            if (model is ISaveable)
+                SaveDataManager.LoadJsonData((ISaveable)model, true);
+        }
+
+        private void Views()
+        {
+            ViewManager.Init();
         }
     }
 }
