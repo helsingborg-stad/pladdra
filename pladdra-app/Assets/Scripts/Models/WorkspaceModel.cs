794a706cca731b2a878f1dbad83827fa667a2e4a
nRamstedt <nikolas.ramstedt@gmail.com>
2021-09-10 15:09:42 +0200

    add crud ops & useful events

--- pladdra-app/Assets/Scripts/Models/WorkspaceModel.cs
+++ pladdra-app/Assets/Scripts/Models/WorkspaceModel.cs
@@ -7,15 +7,95 @@ using UnityEngine;
 
 using Newtonsoft.Json;
 
-using Pladdra;
-using Pladdra.Core;
-
 namespace Pladdra.MVC.Models
 {
     [System.Serializable]
-    public class WorkspaceModel : Pladdra.API.Types.Workspace
+    public class WorkspaceModel : Pladdra.Core.Types.Workspace
     {
+        public event BlockEventHandler OnBlockCreated;
+        public event BlockEventHandler OnBlockDeleted;
+        public event BlockEventHandler OnBlockPositionChanged;
+        public event BlockEventHandler OnBlockRotationChanged;
+        public delegate void BlockEventHandler(string id);
+
         private Savable<WorkspaceModel> savable;
+
+        private List<Pladdra.Core.Types.Block> items
+        {
+            get
+            {
+                if (blocks == null)
+                {
+                    blocks = new Pladdra.Core.Types.ModelBlockConnection();
+                    blocks.items = new List<Pladdra.Core.Types.Block>();
+                }
+
+                return blocks.items;
+            }
+
+            set { blocks.items = value; }
+        }
+
+        public Pladdra.Core.Types.Block GetBlock(string id)
+        {
+            List<Pladdra.Core.Types.Block> item = items.Where(item => item.id == id).ToList();
+            return item[0];
+        }
+
+        public List<Pladdra.Core.Types.Block> ListBlocks() => items;
+
+        public void CreateBlock(Core.Types.CreateBlockInput input, out Pladdra.Core.Types.Block createdItem)
+        {
+            string serializedJson = JsonConvert.SerializeObject(input);
+            createdItem = (Pladdra.Core.Types.Block)JsonConvert.DeserializeObject<Pladdra.Core.Types.Block>(serializedJson);
+            items.Insert(0, createdItem);
+        }
+
+        public void CreateBlock(Core.Types.CreateBlockInput input)
+        {
+            CreateBlock(input, out Pladdra.Core.Types.Block createdItem);
+            if (OnBlockCreated != null)
+                OnBlockCreated(createdItem.id);
+        }
+
+        public void UpdateBlock(Core.Types.UpdateBlockInput input)
+        {
+            string serializedJson = JsonConvert.SerializeObject(input);
+            Pladdra.Core.Types.Block updatedItem = JsonConvert.DeserializeObject<Pladdra.Core.Types.Block>(serializedJson);
+            Pladdra.Core.Types.Block match = items.Find(item => item.id.Contains(updatedItem.id));
+
+            items.ForEach(item =>
+            {
+                if (item.id == updatedItem.id)
+                {
+                    if (updatedItem.position != item.position)
+                    {
+                        item.position = updatedItem.position;
+                        if (OnBlockPositionChanged != null)
+                            OnBlockPositionChanged(item.id);
+                    }
+
+                    if (updatedItem.rotation != item.rotation)
+                    {
+                        item.rotation = updatedItem.rotation;
+                        if (OnBlockRotationChanged != null)
+                            OnBlockRotationChanged(item.id);
+                    }
+
+                    // Additional changes to notify
+                    // 
+                }
+            });
+        }
+
+        public void DeleteBlock(string id)
+        {
+            List<Core.Types.Block> updatedItems = items.Where(item => item.id != id).ToList();
+            items = updatedItems;
+
+            if (OnBlockDeleted != null)
+                OnBlockDeleted(id);
+        }
 
         public void Load()
         {
@@ -36,6 +116,11 @@ namespace Pladdra.MVC.Models
 
             savable.fileName = "workspaces/" + (id ?? "workspace") + ".json";
             savable.instance = this;
+        }
+
+        public string UUID()
+        {
+            return System.Guid.NewGuid().ToString();
         }
     }
 }
