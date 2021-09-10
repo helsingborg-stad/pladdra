489309a8dcbc647f95cb9f31aa308ab28bc340dd
nRamstedt <nikolas.ramstedt@gmail.com>
2021-09-10 15:05:32 +0200

    add modified remote types

--- pladdra-app/Assets/Scripts/Core/Types.cs
+++ pladdra-app/Assets/Scripts/Core/Types.cs
@@ -1,10 +1,57 @@
-using System;
 using System.Collections.Generic;
+using UnityEngine;
 using Newtonsoft.Json;
-using GraphQL;
+
 
 namespace Pladdra.Core.Types
 {
+    public class Asset : API.Types.Asset
+    {
+        [JsonProperty("previewTexturePath")]
+        public string previewTexturePath;
+
+        [JsonProperty("meshPath")]
+        public string meshPath;
+
+        [JsonProperty("prefabPath")]
+        public string prefabPath;
+    }
+
+    public class Block : Pladdra.API.Types.Block
+    {
+        [JsonProperty("position")]
+        public new Vector3 position { get; set; }
+
+        [JsonProperty("rotation")]
+        public new Quaternion rotation { get; set; }
+    }
+
+    public class CreateBlockInput : Pladdra.API.Types.CreateBlockInput
+    {
+        [JsonRequired]
+        public new Vect3Input position { get; set; }
+
+        [JsonRequired]
+        public new QuatInput rotation { get; set; }
+    }
+
+    public class UpdateBlockInput : Pladdra.API.Types.CreateBlockInput
+    {
+        public new Vect3 position { get; set; }
+
+        public new Quat rotation { get; set; }
+    }
+
+    public class Workspace : Pladdra.API.Types.Workspace
+    {
+        [JsonProperty("blocks")]
+        public new ModelBlockConnection blocks { get; set; }
+    }
+
+    public class ModelBlockConnection : Pladdra.API.Types.ModelBlockConnection
+    {
+        public new List<Block> items { get; set; }
+    }
     public class Quat : Pladdra.API.Types.Quat
     {
         #region members
@@ -22,6 +69,21 @@ namespace Pladdra.Core.Types
         #endregion
     }
 
+    public class QuatInput : Pladdra.API.Types.QuatInput
+    {
+        [JsonRequired]
+        public new float x { get; set; }
+
+        [JsonRequired]
+        public new float y { get; set; }
+
+        [JsonRequired]
+        public new float z { get; set; }
+
+        [JsonRequired]
+        public new float w { get; set; }
+    }
+
     public class Vect3 : Pladdra.API.Types.Vect3
     {
         #region members
@@ -36,24 +98,17 @@ namespace Pladdra.Core.Types
         #endregion
     }
 
-    public class Block : Pladdra.API.Types.Block
+    public class Vect3Input : Pladdra.API.Types.Vect3Input
     {
-        [JsonProperty("position")]
-        public new Vect3 position { get; set; }
+        #region members
+        [JsonRequired]
+        public new float x { get; set; }
 
-        [JsonProperty("rotation")]
-        public new Quat rotation { get; set; }
-    }
+        [JsonRequired]
+        public new float y { get; set; }
 
-    public class Workspace : Pladdra.API.Types.Workspace
-    {
-
-        [JsonProperty("blocks")]
-        public new ModelBlockConnection blocks { get; set; }
-    }
-
-    public class ModelBlockConnection : Pladdra.API.Types.ModelBlockConnection
-    {
-        public new List<Block> items { get; set; }
+        [JsonRequired]
+        public new float z { get; set; }
+        #endregion
     }
 }
