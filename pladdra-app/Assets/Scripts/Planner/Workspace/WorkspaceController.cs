39183851730249ee7fb12bf4a767f4d6ea004bf3
nRamstedt <nikolas.ramstedt@gmail.com>
2021-09-10 15:12:21 +0200

    add workspace controller & subscribe transform/scale events

--- pladdra-app/Assets/Scripts/Planner/Workspace/WorkspaceController.cs
+++ pladdra-app/Assets/Scripts/Planner/Workspace/WorkspaceController.cs
@@ -20,8 +20,65 @@ namespace Pladdra.MVC.Controllers
             }
         }
 
+        private GameObject workspaceObject;
+        private WorkspaceView workspaceView;
+
         public WorkspaceController()
         {
+            workspaceObject = GameObject.Find("Workspace");
+            workspaceView = workspaceObject.GetComponent<WorkspaceView>();
+            context.Init += OnPlannerInit;
+
+            context.grid.OnVisibleChanged += () => workspaceView.blocksRootObject.SetActive(context.grid.visible);
+            context.grid.OnSizeChanged += SetBoxCollider;
+            context.grid.OnPositionChanged += TransformWorkspace;
+            context.grid.OnScaleChanged += ScaleWorkspace;
+            context.grid.OnRotationChanged += RotateWorkspace;
+            context.grid.OnIsSelectableChanged += () => workspaceView.leanSelectable.enabled = context.grid.isSelectable;
+
+            workspaceView.Initialize();
+        }
+
+        public void ScaleWorkspace()
+        {
+            workspaceObject.transform.localScale = new Vector3(context.grid.scale, context.grid.scale, context.grid.scale);
+        }
+        public void TransformWorkspace()
+        {
+            workspaceObject.transform.position = new Vector3(context.grid.position.X, context.grid.position.Y, context.grid.position.Z);
+        }
+        public void RotateWorkspace()
+        {
+            // To be continued
+        }
+
+        private void OnPlannerInit()
+        {
+            context.workspace.OnBlockCreated += HandleBlockCreated;
+        }
+
+        private void HandleBlockCreated(string id)
+        {
+            Debug.Log("HandleBlockCreated");
+            var createdBlock = context.workspace.GetBlock(id);
+            workspaceView.InstantiateBlock(createdBlock, context.assets.Get(createdBlock.assetID));
+        }
+
+        public void DestroyHandler()
+        {
+            var blocks = context.workspace.ListBlocks();
+            if (blocks.Count == 0)
+                return;
+
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                workspaceView.RemoveBlock(blocks[i].id);
+            }
+        }
+
+        public void SetBoxCollider()
+        {
+            workspaceView.boxCollider.size = new Vector3(context.grid.size.X * context.grid.size.Z, 0.1f, context.grid.size.X * context.grid.size.Z);
         }
     }
 }
