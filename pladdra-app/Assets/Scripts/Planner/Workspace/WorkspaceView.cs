cbc2875b2dc06b83f5944005a2ea7119655d473f
nRamstedt <nikolas.ramstedt@gmail.com>
2021-09-10 15:10:35 +0200

    add workspace view

--- pladdra-app/Assets/Scripts/Planner/Workspace/WorkspaceView.cs
+++ pladdra-app/Assets/Scripts/Planner/Workspace/WorkspaceView.cs
@@ -16,23 +16,54 @@ namespace Pladdra.MVC.Views
 {
     using Lean.Common;
     using Lean.Touch;
+    using Pladdra.Core.Types;
 
+    [RequireComponent(typeof(LeanSelectable), typeof(BoxCollider))]
     public class WorkspaceView : View
     {
+        private Block createdBlock;
+
+        public GameObject blocksRootObject;
+
+        public IDictionary<string, GameObject> blocks = new Dictionary<string, GameObject>();
+
+        public LeanSelectable leanSelectable;
+        public BoxCollider boxCollider;
+
         public override void Initialize()
         {
+            leanSelectable = GetComponent<LeanSelectable>();
+            boxCollider = GetComponent<BoxCollider>();
         }
 
         void Update()
         {
+            if (createdBlock != null)
+            {
+
+            }
         }
 
-        private void InstantiateBlock(string asset)
+        public void InstantiateBlock(Block block, Asset asset)
         {
+            createdBlock = block;
+            PigletImporter.import(App.CachePath + '/' + asset.meshPath, OnCompleteImport);
         }
 
-        private void RemoveBlock(string id)
+        public void OnCompleteImport(GameObject importedBlock)
         {
+            blocks.Add(createdBlock.id, importedBlock);
+            importedBlock.transform.SetParent(blocksRootObject.transform, false);
+            importedBlock.SetActive(true);
+            createdBlock = null;
+        }
+
+        public void RemoveBlock(string id)
+        {
+            if (!blocks.ContainsKey(id))
+                return;
+            Destroy(blocks[id]);
+            blocks.Remove(id);
         }
     }
 }
