0b367f0b6793660793972b059b86581b98b25902
nRamstedt <nikolas.ramstedt@gmail.com>
2021-09-14 15:11:11 +0200

    add event handlers

--- pladdra-app/Assets/Scripts/Core/View/View.cs
+++ pladdra-app/Assets/Scripts/Core/View/View.cs
@@ -3,10 +3,26 @@ using UnityEngine.Events;
 
 public abstract class View : MonoBehaviour
 {
+    public event ViewEventHandler OnInit;
+    public event ViewEventHandler OnShow;
+    public event ViewEventHandler OnHide;
+
     public virtual void _Initialize() { }
     public abstract void Initialize();
 
-    public virtual void Hide() => gameObject.SetActive(false);
+    public virtual void Hide()
+    {
+        gameObject.SetActive(false);
+        if (OnHide != null)
+            OnHide();
+    }
 
-    public virtual void Show() => gameObject.SetActive(true);
+    public virtual void Show()
+    {
+        gameObject.SetActive(true);
+        if (OnShow != null)
+            OnShow();
+    }
+
+    public delegate void ViewEventHandler();
 }
