95fd13428abdb498fb789ccc7a6cc7dcdf724c92
nRamstedt <nikolas.ramstedt@gmail.com>
2021-09-21 09:51:03 +0200

    implement scale button functionality

--- pladdra-app/Assets/Scripts/Planner/States/WorkspaceSelection.cs
+++ pladdra-app/Assets/Scripts/Planner/States/WorkspaceSelection.cs
@@ -14,27 +14,66 @@ namespace Pladdra
     {
         public PlannerController planner { get; set; }
 
+        bool isMinScale
+        {
+            get
+            {
+                return planner.context.grid.scale == planner.context.grid.minScale;
+            }
+        }
+
+
+        string scaleButtonText
+        {
+            get
+            {
+                if (isMinScale)
+                {
+                    return "Skala upp";
+                }
+                return "Skala ner";
+            }
+        }
+
         public void OnMount()
         {
+            planner.context.grid.OnScaleChanged += SetScaleButtonText;
             planner.plannerGUI.editGridGUI.removeGridButton.onClick.AddListener(OnClickRemoveGrid);
             planner.plannerGUI.editGridGUI.lockGridButton.onClick.AddListener(OnClickLockGrid);
+            planner.plannerGUI.editGridGUI.scaleGridButton.onClick.AddListener(OnClickScaleGrid);
+
 
             planner.context.grid.visible = true;
             planner.context.ar.planeDetection = true;
             planner.context.grid.isSelectable = true;
 
+            SetScaleButtonText();
             planner.plannerGUI.editGridGUI.Show();
+        }
+
+        private void SetScaleButtonText()
+        {
+            planner.plannerGUI.editGridGUI.scaleGridButtonText.text = scaleButtonText;
         }
 
         public void OnUnmount()
         {
+            planner.context.grid.OnScaleChanged -= SetScaleButtonText;
             planner.plannerGUI.editGridGUI.removeGridButton.onClick.RemoveListener(OnClickRemoveGrid);
             planner.plannerGUI.editGridGUI.lockGridButton.onClick.RemoveListener(OnClickLockGrid);
+            planner.plannerGUI.editGridGUI.scaleGridButton.onClick.RemoveListener(OnClickScaleGrid);
 
             planner.context.ar.planeDetection = false;
             planner.context.grid.isSelectable = false;
 
             planner.plannerGUI.editGridGUI.Hide();
+        }
+
+        public void OnClickScaleGrid()
+        {
+            planner.context.grid.scale = isMinScale
+            ? planner.context.grid.maxScale
+            : planner.context.grid.minScale;
         }
 
         public void OnClickLockGrid()
