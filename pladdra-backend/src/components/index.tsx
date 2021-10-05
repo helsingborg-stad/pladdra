e92564c4e4e8ece1b727c86efeb65b6ccd427ac3
nRamstedt <nikolas.ramstedt@gmail.com>
2021-10-05 09:57:13 +0200

    export components

---
+++ pladdra-backend/src/components/index.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
+export { default as Asset } from "@components/Asset";
+export { default as Dialogue } from "@components/Dialogue";
+export { default as LoginPage } from "@components/LoginPage";
