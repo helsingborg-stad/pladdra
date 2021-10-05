71b06624c407bbb058727a14686ce07f2d66e27a
nRamstedt <nikolas.ramstedt@gmail.com>
2021-10-05 09:59:11 +0200

    implement translation n stuff in app

--- pladdra-backend/src/App.tsx
+++ pladdra-backend/src/App.tsx
@@ -1,37 +1,82 @@
 import React from "react";
 import Amplify from "aws-amplify";
-import { Resource } from "react-admin";
-import { AmplifyAdmin } from "react-admin-amplify";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import { AssetList } from "components/Asset/List";
-import { EditAsset } from "components/Asset/Edit";
-import { CreateAsset } from "components/Asset/Create";
-import * as mutations from "graphql/mutations";
-import * as queries from "graphql/queries";
+import { Admin, Resource } from "react-admin";
+import {
+  CognitoGroupList,
+  CognitoUserList,
+  CognitoUserShow,
+  buildAuthProvider,
+  buildDataProvider,
+} from "react-admin-amplify";
 
-import awsExports from "aws-exports";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { Person, People, PermMedia } from "@material-ui/icons";
 
+import * as mutations from "@app/graphql/mutations";
+import * as queries from "@app/graphql/queries";
+
+import awsExports from "@app/aws-exports";
+import i18nProvider from "@app/i18n/i18nProvider";
+// import customRoutes from "@app/customRoutes";
+import theme from "@theme";
+import { Asset, Dialogue, LoginPage } from "@components";
 // import Dashboard from './components/Dashboard/Dashboard'
 
-Amplify.configure({...awsExports, Auth: {authenticationFlowType: 'USER_PASSWORD_AUTH'}});
+Amplify.configure({
+  ...awsExports,
+  Auth: { authenticationFlowType: "USER_PASSWORD_AUTH" },
+});
 
 const App = (): React.ReactElement => (
-  <AmplifyAdmin
-    // dashboard={Dashboard}
-    operations={{ queries, mutations }}
-    options={{
-      authGroups: ["superadmin", "admin"],
-      storageBucket: awsExports.aws_user_files_s3_bucket,
-      storageRegion: awsExports.aws_user_files_s3_bucket_region,
-    }}
-  >
-    <Resource
-      name="assets"
-      list={AssetList}
-      create={CreateAsset}
-      edit={EditAsset}
-    />
-  </AmplifyAdmin>
+  <ThemeProvider theme={theme}>
+    <Admin
+      disableTelemetry
+      authProvider={buildAuthProvider({ authGroups: ["superadmin", "admin"] })}
+      dataProvider={buildDataProvider(
+        { queries, mutations },
+        {
+          storageBucket: awsExports.aws_user_files_s3_bucket,
+          storageRegion: awsExports.aws_user_files_s3_bucket_region,
+          enableAdminQueries: true,
+        }
+      )}
+      loginPage={LoginPage}
+      theme={theme}
+      i18nProvider={i18nProvider}
+    >
+      <Resource
+        name="dialogues"
+        list={Dialogue.List}
+        create={Dialogue.Create}
+        edit={Dialogue.Edit}
+        options={{}}
+      />
+
+      <Resource
+        name="assets"
+        list={Asset.List}
+        create={Asset.Create}
+        edit={Asset.Edit}
+        icon={PermMedia}
+        options={{}}
+      />
+
+      <Resource
+        name="cognitoUsers"
+        list={CognitoUserList}
+        show={CognitoUserShow}
+        icon={Person}
+        options={{}}
+      />
+
+      <Resource
+        name="cognitoGroups"
+        list={CognitoGroupList}
+        icon={People}
+        options={{}}
+      />
+    </Admin>
+  </ThemeProvider>
 );
 
 export default App;
